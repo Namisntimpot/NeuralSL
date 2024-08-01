423e1109931fafb5434c16e8fc1d42193af9f0f2
Namisntimpot <137285609+Namisntimpot@users.noreply.github.com>
2024-08-02 00:10:44 +0800

    traditional phase-shift patterns

---
+++ SLPipeline/gen_pattern.py
@@ -0,0 +1,67 @@
+import os
+import numpy as np
+import cv2
+
+# 只考虑黑白...
+class PatternGenerator:
+    def __init__(self, width, height, n_patterns = None, output_dir = None, format = 'png') -> None:
+        self.width = width
+        self.height = height
+        self.n_patterns = n_patterns
+        self.output_dir = output_dir
+        self.format = format
+        
+    def gen_pattern(self, save = True):
+        '''
+        返回元组, 分别是pattern的数组和 code matrix
+        '''
+        if save:
+            assert self.output_dir is not None
+
+    def save_all_to_dir(self, patterns:list[np.ndarray], codematrix:np.ndarray):
+        for i in range(len(patterns)):
+            pat = patterns[i]
+            self.save_to_dir(pat, i)
+        self.save_to_dir(codematrix, 'code matrix')
+
+    def save_to_dir(self, img:np.ndarray, index:int | str):
+        '''
+        认为img在0-1范围内.
+        index为str表示是code matrix
+        '''
+        if not self.format in ['tif', 'exr']:     # 列举一些浮点数图片格式
+            normalized = (img * 255).astype(np.uint8)
+        else:
+            normalized = img
+        if not os.path.exists(self.output_dir):
+            os.mkdir(self.output_dir)
+        if type(index) == str:
+            cv2.imwrite(os.path.join(self.output_dir, "code_matrix."+self.format), normalized)
+        else:
+            cv2.imwrite(os.path.join(self.output_dir, "%04d."%(index) + self.format), normalized)
+
+
+class SinPhaseShiftPattern(PatternGenerator):
+    def __init__(self, width, height, n_patterns=None, output_dir=None, format='png',minF = 1, maxF = 8, n_shifts = 4) -> None:
+        super().__init__(width, height, (maxF - minF + 1) * n_shifts, output_dir, format)
+        self.minF = minF
+        self.maxF = maxF
+        self.n_shifts = n_shifts
+
+
+    def gen_pattern(self, save=True):
+        super().gen_pattern(save)
+        frequencies = np.arange(self.minF, self.maxF + 1)  # 频率从1到16
+        phase_shifts = np.linspace(0, 2 * np.pi, self.n_shifts, endpoint=False)  # 10次相位移动
+        patterns = []
+        code_matrix = np.zeros((self.n_patterns, self.width))
+        for freq in frequencies:
+            for phase in phase_shifts:
+                x = np.linspace(0, 2 * np.pi * freq, self.width)
+                y = (1 + np.cos(x + phase)) / 2  # 正弦波图案
+                pattern = np.tile(y, (self.height, 1))  # 重复图案
+                code_matrix[len(pattern), :] = y
+                patterns.append(pattern)
+        if save:
+            self.save_all_to_dir(patterns, code_matrix)
+        return patterns, code_matrix
